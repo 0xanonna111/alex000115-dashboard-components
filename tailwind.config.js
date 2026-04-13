9ab519ae23fef10ca4533a5ca5d104eda97cf4ae
Anonna <136353692+0xanonna111@users.noreply.github.com>
2026-04-13 20:17:51 +0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        zinc: {
+          950: '#09090b',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
